51ac10270467e663e33a83650506a07dd7748c0f
Harsh Prajapati <harshprajapaticoding@gmail.com>
2026-01-22 05:35:02 +0530

    added the server url in env

--- frontend/src/api/serverApi.ts
+++ frontend/src/api/serverApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = "http://localhost:3000";
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 const api = axios.create({
   baseURL: SERVER_URL,
   withCredentials: true,
